05cd1b2049ceebc4218b83c40b14b57dc6b1bcd3
Tammy <tammyanndo@gmail.com>
2021-11-14 13:46:39 -0500

    added features to admin dashboard to include number of categories and total products in database; also added clickable link to admin product page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,8 @@
 class Admin::DashboardController < ApplicationController
+
   def show
+    # @category = Category.order(id: :desc).all
+    @category = Category.all
+    @products = Product.order(id: :desc).all
   end
 end
